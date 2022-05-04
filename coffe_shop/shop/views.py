68274926de134466adc30aa61f4ffe4aee3abef9
Nicolai Pavliuc <pavliuc75@gmail.com>
2022-05-04 16:18:06 +0200

    i dex upd

--- coffe_shop/shop/views.py
+++ coffe_shop/shop/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
 
 # Create your views here.
+from shop.models import *
+
+items = [Drink(DrinkTypes.Tea, TeaTypes.Green, DrinkSizes.Small),
+         Drink(DrinkTypes.Tea, TeaTypes.Black, DrinkSizes.Small),
+         Drink(DrinkTypes.Tea, TeaTypes.Mint, DrinkSizes.Small),
+         Drink(DrinkTypes.Coffee, CoffeeTypes.Espresso, DrinkSizes.Small),
+         Drink(DrinkTypes.Coffee, CoffeeTypes.Latte, DrinkSizes.Small),
+         Drink(DrinkTypes.Coffee, CoffeeTypes.Americano, DrinkSizes.Small),
+         Drink(DrinkTypes.Juice, JuiceType.Apple, DrinkSizes.Small),
+         Drink(DrinkTypes.Juice, JuiceType.Carrot, DrinkSizes.Small),
+         Drink(DrinkTypes.Juice, JuiceType.Orange, DrinkSizes.Small)]
+
+
 def index(request):
-    return render(request, 'index.html')
+    return render(request, 'index.html', {'items': items})
