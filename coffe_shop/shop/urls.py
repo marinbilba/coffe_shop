4e8de9ea38bc492b68d658a38e549710e98c4502
Nicolai Pavliuc <pavliuc75@gmail.com>
2022-05-04 22:05:14 +0200

    ui updates

--- coffe_shop/shop/urls.py
+++ coffe_shop/shop/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 
 from . import views
 
-
 urlpatterns = [
     path('', views.index, name='index'),
+    path('add_to_cart/<str:price>', views.add_to_cart, name='add_to_cart'),
+    path('reset_price', views.reset_price, name='reset_price'),
 ]
--- coffe_shop/shop/views.py
+++ coffe_shop/shop/views.py
@@ -1,18 +1,32 @@
+from django.http import JsonResponse
 from django.shortcuts import render
 
-# Create your views here.
 from shop.models import *
 
-items = [Drink(DrinkTypes.Tea, TeaTypes.Green, DrinkSizes.Small),
+items = [Drink(DrinkTypes.Tea, TeaTypes.Green, DrinkSizes.Medium),
          Drink(DrinkTypes.Tea, TeaTypes.Black, DrinkSizes.Small),
-         Drink(DrinkTypes.Tea, TeaTypes.Mint, DrinkSizes.Small),
-         Drink(DrinkTypes.Coffee, CoffeeTypes.Espresso, DrinkSizes.Small),
+         Drink(DrinkTypes.Tea, TeaTypes.Mint, DrinkSizes.Large),
+         Drink(DrinkTypes.Coffee, CoffeeTypes.Espresso, DrinkSizes.Large),
          Drink(DrinkTypes.Coffee, CoffeeTypes.Latte, DrinkSizes.Small),
          Drink(DrinkTypes.Coffee, CoffeeTypes.Americano, DrinkSizes.Small),
          Drink(DrinkTypes.Juice, JuiceType.Apple, DrinkSizes.Small),
-         Drink(DrinkTypes.Juice, JuiceType.Carrot, DrinkSizes.Small),
-         Drink(DrinkTypes.Juice, JuiceType.Orange, DrinkSizes.Small)]
+         Drink(DrinkTypes.Juice, JuiceType.Carrot, DrinkSizes.Medium),
+         Drink(DrinkTypes.Juice, JuiceType.Orange, DrinkSizes.Large)]
+
+total_price = 0.0
 
 
 def index(request):
-    return render(request, 'index.html', {'items': items})
+    return render(request, 'index.html', {'items': items, 'total_price': total_price})
+
+
+def add_to_cart(request, price):
+    global total_price
+    total_price += float(price)
+    return JsonResponse({'text': 'Just rendering some JSON :)'})
+
+
+def reset_price(request):
+    global total_price
+    total_price = 0.0
+    return JsonResponse({'text': 'Jus rendering some JSON :)'})
